6e9a1f905fa77b79ed15edf2f4e5b7e4e5100a34
aldinhalilovic <aldinhalilovic06@gmail.com>
2023-04-03 11:24:46 +0200

    conditional, for loop

---
+++ JS/app.js
@@ -0,0 +1,108 @@
+// "stirng",023983,true,false,
+
+// {}, [],
+
+// "3" == 3;
+// "3" === 3;
+
+// && ||
+
+// console.log(3 || 4 || 5);
+// console.log(4 && 5);
+
+// var a = 10;
+// var b;
+
+// b = 15;
+// let;
+// const;
+
+// console.log(a);
+
+// let b = 1;
+// if (b > 7) {
+//   console.log("b je vece od 7");
+// } else if (b > 3) {
+//   console.log("b je vece od 3 ali ne i od 7");
+// } else {
+
+// }
+
+// let a = 3;
+
+// switch (a) {
+//   case 3:
+//     console.log("A je vece od 2");
+//     break;
+//   case 4:
+//     console.log("A je 4");
+//     break;
+//   case 5:
+//     console.log("A je 5");
+//     break;
+//   case 3:
+//     console.log("A je 3");
+//     break;
+//   case 3:
+//     console.log("A je 3");
+//     break;
+//   case 3:
+//     console.log("A je 3");
+//     break;
+//   case 3:
+//     console.log("A je 3");
+//     break;
+//   case 3:
+//     console.log("A je 3");
+//     break;
+
+//   default:
+//     console.log("A nije 3");
+//     break;
+// }
+
+// let c = 10;
+
+// c > 5 ? console.log("vece od 5") : console.log("manje od 5");
+
+// let a = 15;
+// console.log(a);
+// a++;
+// console.log(a);
+// a--;
+// console.log(a);
+// a = a + 5;
+// a += 5;
+// console.log(a);
+
+// let b = 10;
+
+// for (let i = 0; i < 10; i++) {
+//   //   console.log("OVO JE PETLJA", i);
+//   console.log(b, "default");
+//   b += 2;
+//   console.log(b, "petlja");
+//   console.log(i);
+// }
+
+// let a = prompt("Unesi nesto");
+// console.log(+a);
+// console.log(typeof +a);
+// '3', 3
+// alert("Unesi nesto!!!!");
+
+// let brojOkretanja = prompt("Unesi broj okretanja petlje: ");
+
+let a = 0;
+for (let i = 0; i < 3; i++) {
+  //   console.log("nesto", i);
+  a += +prompt("Dodaj broj");
+  console.log(a, i);
+}
+
+// +"3"
+console.log(a);
+// "0" + +"5" + +"5" + +"5"
+// 0+5+5+5
+// let nekiBroj = "5" + "tri";
+// console.log(nekiBroj);
